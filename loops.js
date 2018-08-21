07c0989310653752dae5d3b30d6e82ffda369101
Cheol Noh <dan.noh@gmail.com>
2018-08-21 20:09:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,22 +1,22 @@
 function forLoop(array){
   for(let i = 0; i < 25; i++) {
     if (i === 1) {
-      array[i] = 'I am 1 strange loop.';
+      array.push('I am 1 strange loop.');
     }
     
     else if (i === 11) {
-      array[i] = 'I am 1 strange loop.';
+      array.push('I am 1 strange loop.');
     }
     
     else {
-      array[i] = `I am ${i} strange loops.`;
+      array.push(I`I am ${i} strange loops.`);
     }
     
   }
   return array;
 }
 
-var test = [];
+var test = [1, 2, 3, 4];
 forLoop(test);
 console.log(test[1]);
 console.log(test[24]);
